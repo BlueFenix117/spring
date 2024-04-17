68a8ce18a1a49525de217c46925912840bb0b813
Sebastian Espejel Moran <sebas_esp99@outlook.com>
2024-04-17 17:17:33 -0600

    Se agrego el controlador Persona con sus respectivas reglas de negocio

--- src/main/java/com/curso/spring/controller/FigurasController.java
+++ src/main/java/com/curso/spring/controller/FigurasController.java
@@ -1,17 +1,14 @@
 package com.curso.spring.controller;
 
-import com.curso.spring.dto.Persona;
+
 import com.curso.spring.dto.RectanguloDto;
 import com.curso.spring.dto.TrianguloDto;
 import com.curso.spring.service.RectanguloService;
 import com.curso.spring.service.TrianguloService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("Figuras")
---
+++ src/main/java/com/curso/spring/dto/request/PersonaRequest.java
@@ -0,0 +1,29 @@
+package com.curso.spring.dto.request;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+public class PersonaRequest {
+
+    private Long personaId;
+
+
+    private String nombre;
+
+
+    private Integer edad;
+
+
+    private String genero;
+
+
+    private Integer direccionId;
+
+
+    private Integer empleoId;
+}
