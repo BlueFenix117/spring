d7b946518c6cb99f3ae536231a6e29a0e68f1158
Sebastian Espejel Moran <sebas_esp99@outlook.com>
2024-04-18 13:06:57 -0600

    Se completo el CRUD para Personas

--- src/main/java/com/curso/spring/controller/PersonaController.java
+++ src/main/java/com/curso/spring/controller/PersonaController.java
@@ -1,7 +1,9 @@
 package com.curso.spring.controller;
 
 import com.curso.spring.dto.request.PersonaRequest;
+import com.curso.spring.dto.response.DatosPersonaResponse;
 import com.curso.spring.model.Personas;
+import com.curso.spring.repository.PersonasRepository;
 import com.curso.spring.service.PersonaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/personas")
@@ -35,5 +38,37 @@ public class PersonaController {
     @Operation(summary = "Operacion para guardar los datos de personas")
     public ResponseEntity<Personas> guardarPersonasAlternativo(@RequestBody PersonaRequest personas){
         return personaService.guardarPersonaAlternativo(personas);
+    }
+
+    @GetMapping("/getPersonas/{Id}")
+    @Operation(summary = "Operacion para obtener los datos de personas por id")
+    public Personas buscarPersonaId(@PathVariable Long id){
+        return personaService.buscarPersonaId(id);
+    }
+
+    @PutMapping("/putPersonas")
+    @Operation(summary = "Operacion para actualizar los datos de personas")
+    public ResponseEntity<Object> updatePersona(@RequestBody PersonaRequest request){
+
+        return personaService.updatePersona(request);
+    }
+
+    @DeleteMapping("/deletePersonas/{id}")
+    @Operation(summary = "Operacion para eliminar los datos de personas por id")
+    public void eliminarPersona(@PathVariable Long id){
+        personaService.eliminarPersona(id);
+    }
+
+    @GetMapping("/getPersonas/genero/{genero}")
+    @Operation(summary = "Operacion para obtener los nombres de personas por genero")
+    public List<String> getNombrePersonaByGenero(@PathVariable String genero){
+        return personaService.personasByGenero(genero);
+    }
+
+
+    @GetMapping("/getPersonas/info/{id}")
+    @Operation(summary = "Operacion para obtener la informacion de personas por Id")
+    public ResponseEntity<Object> getInfoPersonas(@PathVariable Integer id){
+        return personaService.getInfoPersonas(id);
     }
 }
--- src/main/java/com/curso/spring/service/PersonaService.java
+++ src/main/java/com/curso/spring/service/PersonaService.java
@@ -1,10 +1,12 @@
 package com.curso.spring.service;
 
 import com.curso.spring.dto.request.PersonaRequest;
+import com.curso.spring.dto.response.DatosPersonaResponse;
 import com.curso.spring.model.Personas;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface PersonaService {
 
@@ -12,4 +14,14 @@ public interface PersonaService {
 
     ResponseEntity<Personas> guardarPersona(Personas personas);
     ResponseEntity<Personas> guardarPersonaAlternativo(PersonaRequest request);
+
+    Personas buscarPersonaId(Long id);
+
+    ResponseEntity<Object> updatePersona(PersonaRequest request);
+
+    void eliminarPersona(Long id);
+
+    List<String> personasByGenero(String genero);
+
+    ResponseEntity<Object> getInfoPersonas(Integer id);
 }
